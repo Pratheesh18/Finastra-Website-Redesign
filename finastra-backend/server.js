e584d1c058962b7e247f09a1ad16aabe1f08f0d2
Pratheesh Balachandran <Pratheesh.Balachandran@cube.global>
2025-05-04 22:46:40 +0530

    deleted unwanted code

--- finastra-backend/server.js
+++ finastra-backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://finastra-frontend.onrender.com' // or '*'
+  origin: 'https://finastra-frontend.onrender.com'
 }));
 
 app.use(express.json());
